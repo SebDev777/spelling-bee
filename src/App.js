05a8d3b5fa874153e54c6aff91d9d3b0f86f2c42
Sebastian Meza <sebastiangamers17@gmail.com>
2025-09-30 09:41:41 -0500

    Removed auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState, useEffect } from "react";
 import TopPage from "./layout/TopPage";
 import CenterPage from "./layout/CenterPage";
 
 import "./App.css";
 
 import { MainContextProvider } from "./contexts/MainContext";
-import VerificationPrompt from "./logic/Verification";
 
 function Main() {
   return (
@@ -24,26 +22,10 @@ function Main() {
   );
 }
 
-function WaitingPage() {
-  return (
-    <div class="d-flex justify-content-center">
-      <div class="spinner-border" role="status" style={{width: "10rem", height: "10rem"}}>
-        <span class="visually-hidden">Loading...</span>
-      </div>
-    </div>
-  );
-}
-
 function App() {
-  const [verified, setVerified] = useState(false);
-
-  useEffect(() => {
-    VerificationPrompt(setVerified);
-  }, []);
-
   return (
     <MainContextProvider>
-      {verified ? <Main /> : <WaitingPage />}
+      <Main />
     </MainContextProvider>
   );
 }
